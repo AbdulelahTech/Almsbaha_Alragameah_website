497798a71199f6b612ec466e1ba8567a71cb9189
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-25 19:25:13 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,6 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-        <Image
-        src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"
-        alt="Landscape picture"
-        width={500}
-        height={500}
-      />
         <p className="text-2xl dark:text-white text-center">
           <Link href="/">
             <a>{name}</a>
