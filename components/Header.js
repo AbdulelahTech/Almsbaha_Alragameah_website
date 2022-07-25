1341818b21f85eaa93a83b7040be20368a8640d7
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-25 18:36:53 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,14 @@
 import Link from 'next/link';
-import Image from 'next/image'
+
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-        <Image
-        src={require('https://github.com/AbdulelahTech/Almsbaha_Alragameah_website/blob/main/Logo.jpg').default}
-        alt="Logo Image"
-        width={250}
-        height={250}
-      />
+           <picture>
+            <source srcSet="https://github.com/AbdulelahTech/Almsbaha_Alragameah_website/blob/main/Logo.jpg" type="image/webp" />
+            <img src="https://github.com/AbdulelahTech/Almsbaha_Alragameah_website/blob/main/Logo.jpg" alt="Landscape picture" />
+           </picture>
         <p className="text-2xl dark:text-white text-center">
           <Link href="/">
             <a>{name}</a>
