54e5c4bd6e79bf3e08e6f91e5e702e153f6b6d73
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-25 05:58:53 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,12 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <img src="components/playstore.png" alt="Logo Image" className="text-2xl dark:text-white text-center" />
+      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+        <p className="text-2xl dark:text-white text-center">
+          <Link href="/">
+            <a>{name}</a>
+          </Link>
+        </p>
     </header>
   );
 }
