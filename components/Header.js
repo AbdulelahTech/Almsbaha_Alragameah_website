744507b1ac8a7004b17633a0734d35def19a7889
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-25 18:18:17 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,6 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-          <Img src="https://github.com/AbdulelahTech/Almsbaha_Alragameah_website/blob/main/Logo.jpg" alt="Logo Image"  width={250}
-        height={250} />
         <p className="text-2xl dark:text-white text-center">
           <Link href="/">
             <a>{name}</a>
