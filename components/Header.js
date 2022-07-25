d07745c7987775c306c4c9de325fda50300151d8
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-25 05:50:10 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src="components/playstore.png" alt="Logo Image" className="gallery-img" />
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
-        </Link>
-      </p>
+      <img src="components/playstore.png" alt="Logo Image" className="text-2xl dark:text-white text-center" />
     </header>
   );
 }
