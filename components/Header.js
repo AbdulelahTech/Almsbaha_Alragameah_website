471933abd9d33f8f0a0ca68d25b298fb92a425bb
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-27 23:10:35 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,6 @@ import Logo from './Logo.jpg'
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-//       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
        <Link href="/">
       <Image
         src={Logo}
