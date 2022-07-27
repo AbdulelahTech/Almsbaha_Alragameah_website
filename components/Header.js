cce589228ee17af82b147fe4eb218ebbcbaaba83
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-27 23:19:37 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,16 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
        <Link href="/">
       <Image
-        className="w-12 h-12 rounded-full block mx-12 mb-4"
+        className="w-12 h-12 rounded-full block mx-auto mb-4"
         src={Logo}
         alt="Logo Image"
-//         width={100} 
-//         height={100} 
+        width={100} 
+        height={100} 
         // blurDataURL="data:..." automatically provided
         placeholder="blur" // Optional blur-up while loading
       />
       </Link>
+      <div clasName="w-12 h-12" />
         <p className="text-2xl dark:text-white text-center">
           <Link href="/">
             <a>{name}</a>
