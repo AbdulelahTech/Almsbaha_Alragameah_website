ab140cfa54624d62496ed147e6228b46b7a7d2cd
AbdulelahTech <100416424+AbdulelahTech@users.noreply.github.com>
2022-07-28 02:29:47 +0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Almsbaha Alragmeah';
+    : '';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '';
+    : 'Almsbaha Alragmeah';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved to Abdulelah tech.';
+    : 'All rights reserved to Abdulelah\'s tech.';
 
   return {
     name,
